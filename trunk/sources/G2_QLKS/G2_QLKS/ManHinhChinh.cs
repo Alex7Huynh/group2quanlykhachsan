831e75f9632f8e202e2e173841aff7b711043f77
0812611@gmail.com <0812611@gmail.com@df72b849-1963-1cf4-8301-46637306db84>
2011-04-01 09:35:11 +0000

    Hoàn thành Giao diện Menu chính và Điều chỉh danh mục phòng, AboutBox

--- trunk/sources/G2_QLKS/G2_QLKS/ManHinhChinh.cs
+++ trunk/sources/G2_QLKS/G2_QLKS/ManHinhChinh.cs
@@ -21,42 +21,117 @@ namespace G2_QLKS
 
         }
 
+
+
+
+
+        // Nhấn Button chức năng Điều chỉnh danh mục phòng
         private void simpleButton_DieuChinhDanhMucPhong_Click(object sender, EventArgs e)
         {
             DieuChinhDanhMucPhong frmDieuChinhDanhMucPhong = new DieuChinhDanhMucPhong();
             frmDieuChinhDanhMucPhong.Show();
         }
 
+
+
+
+
+        // Nhấn Button chức năng Đặt phòng
         private void simpleButton_DatPhong_Click(object sender, EventArgs e)
         {
 
         }
 
+
+
+
+        // Chọn Menu Help -> About
         private void toolStripMenuItem_About_Click(object sender, EventArgs e)
         {
             AboutBox dlgAbout = new AboutBox();
             dlgAbout.ShowDialog();
         }
 
+
+
+
+
+        // Chọn Menu Help -> Help
         private void toolStripMenuItem_HelpDiaglog_Click(object sender, EventArgs e)
         {
 
         }
 
+
+
+
+
+        // Nhấn Button Trước trên Toolbar
         private void toolStripButton_Truoc_Click(object sender, EventArgs e)
         {
 
         }
 
+
+
+
+
+        // Nhấn Button Sau trên Toolbar
         private void toolStripButton_Sau_Click(object sender, EventArgs e)
         {
 
         }
 
+
+
+
+
+        // Chọn Menu Exit 
         private void toolStripMenuItem_Exit_Click(object sender, EventArgs e)
         {
             Close();
         }
+
+
+
+
+
+        // Chọn Menu View  -> Toolbar 
+        // Coder : Trần Duy Việt
+        private void toolStripMenuItem_ViewToolbar_Click(object sender, EventArgs e)
+        {
+            if (toolStrip_ToolBarChinh.Visible == true)
+            {
+                toolStripMenuItem_ViewToolbar.Checked = false;
+                toolStrip_ToolBarChinh.Visible = false;
+            }
+            else
+            {
+                toolStripMenuItem_ViewToolbar.Checked = true;
+                toolStrip_ToolBarChinh.Visible = true;
+            }
+        }
+
+
+
+
+        // Chọn Menu View -> Thanh chức năng
+        // Coder : Trần Duy Việt
+        private void toolStripMenuItem_ViewThanhChucNang_Click(object sender, EventArgs e)
+        {
+            if (navBarControl.Visible == true)
+            {
+                toolStripMenuItem_ViewThanhChucNang.Checked = false;
+                navBarControl.Visible = false;
+            }
+            else
+            {
+                toolStripMenuItem_ViewThanhChucNang.Checked = true;
+                navBarControl.Visible = true;
+            }
+        }
+
+        
 
 
      
