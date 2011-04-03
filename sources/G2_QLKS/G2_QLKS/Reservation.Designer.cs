4131c1d759614344198a117cfdef018f34000e44
0812517@gmail.com <0812517@gmail.com@df72b849-1963-1cf4-8301-46637306db84>
2011-04-03 15:38:28 +0000

    thêm button vào form Reservation 0812517

--- sources/G2_QLKS/G2_QLKS/Reservation.Designer.cs
+++ sources/G2_QLKS/G2_QLKS/Reservation.Designer.cs
@@ -28,19 +28,159 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dtpBeginDate = new System.Windows.Forms.DateTimePicker();
+            this.lblFrom = new System.Windows.Forms.Label();
+            this.lblTo = new System.Windows.Forms.Label();
+            this.dtpEndDate = new System.Windows.Forms.DateTimePicker();
+            this.lblDuration = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblTen = new System.Windows.Forms.Label();
+            this.txtTen = new System.Windows.Forms.TextBox();
+            this.lblLoaiPhong = new System.Windows.Forms.Label();
+            this.cboLoaiPhong = new System.Windows.Forms.ComboBox();
+            this.cboPhong = new System.Windows.Forms.ComboBox();
+            this.lblPhong = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // dtpBeginDate
+            // 
+            this.dtpBeginDate.CustomFormat = "DD/MM/YYYY";
+            this.dtpBeginDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpBeginDate.Location = new System.Drawing.Point(78, 145);
+            this.dtpBeginDate.Name = "dtpBeginDate";
+            this.dtpBeginDate.Size = new System.Drawing.Size(96, 20);
+            this.dtpBeginDate.TabIndex = 0;
+            // 
+            // lblFrom
+            // 
+            this.lblFrom.AutoSize = true;
+            this.lblFrom.Location = new System.Drawing.Point(42, 151);
+            this.lblFrom.Name = "lblFrom";
+            this.lblFrom.Size = new System.Drawing.Size(30, 13);
+            this.lblFrom.TabIndex = 1;
+            this.lblFrom.Text = "From";
+            // 
+            // lblTo
+            // 
+            this.lblTo.AutoSize = true;
+            this.lblTo.Location = new System.Drawing.Point(42, 177);
+            this.lblTo.Name = "lblTo";
+            this.lblTo.Size = new System.Drawing.Size(20, 13);
+            this.lblTo.TabIndex = 2;
+            this.lblTo.Text = "To";
+            // 
+            // dtpEndDate
+            // 
+            this.dtpEndDate.CustomFormat = "DD/MM/YYYY";
+            this.dtpEndDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpEndDate.Location = new System.Drawing.Point(78, 171);
+            this.dtpEndDate.Name = "dtpEndDate";
+            this.dtpEndDate.Size = new System.Drawing.Size(96, 20);
+            this.dtpEndDate.TabIndex = 3;
+            // 
+            // lblDuration
+            // 
+            this.lblDuration.AutoSize = true;
+            this.lblDuration.Location = new System.Drawing.Point(202, 152);
+            this.lblDuration.Name = "lblDuration";
+            this.lblDuration.Size = new System.Drawing.Size(47, 13);
+            this.lblDuration.TabIndex = 4;
+            this.lblDuration.Text = "Duration";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(205, 168);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // lblTen
+            // 
+            this.lblTen.AutoSize = true;
+            this.lblTen.Location = new System.Drawing.Point(12, 77);
+            this.lblTen.Name = "lblTen";
+            this.lblTen.Size = new System.Drawing.Size(26, 13);
+            this.lblTen.TabIndex = 6;
+            this.lblTen.Text = "Tên";
+            // 
+            // txtTen
+            // 
+            this.txtTen.Location = new System.Drawing.Point(78, 74);
+            this.txtTen.Name = "txtTen";
+            this.txtTen.Size = new System.Drawing.Size(100, 20);
+            this.txtTen.TabIndex = 7;
+            // 
+            // lblLoaiPhong
+            // 
+            this.lblLoaiPhong.AutoSize = true;
+            this.lblLoaiPhong.Location = new System.Drawing.Point(11, 15);
+            this.lblLoaiPhong.Name = "lblLoaiPhong";
+            this.lblLoaiPhong.Size = new System.Drawing.Size(61, 13);
+            this.lblLoaiPhong.TabIndex = 8;
+            this.lblLoaiPhong.Text = "Loại Phòng";
+            // 
+            // cboLoaiPhong
+            // 
+            this.cboLoaiPhong.FormattingEnabled = true;
+            this.cboLoaiPhong.Location = new System.Drawing.Point(78, 12);
+            this.cboLoaiPhong.Name = "cboLoaiPhong";
+            this.cboLoaiPhong.Size = new System.Drawing.Size(121, 21);
+            this.cboLoaiPhong.TabIndex = 9;
+            // 
+            // cboPhong
+            // 
+            this.cboPhong.FormattingEnabled = true;
+            this.cboPhong.Location = new System.Drawing.Point(311, 12);
+            this.cboPhong.Name = "cboPhong";
+            this.cboPhong.Size = new System.Drawing.Size(121, 21);
+            this.cboPhong.TabIndex = 11;
+            // 
+            // lblPhong
+            // 
+            this.lblPhong.AutoSize = true;
+            this.lblPhong.Location = new System.Drawing.Point(244, 15);
+            this.lblPhong.Name = "lblPhong";
+            this.lblPhong.Size = new System.Drawing.Size(38, 13);
+            this.lblPhong.TabIndex = 10;
+            this.lblPhong.Text = "Phòng";
             // 
             // frmReservation
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(600, 261);
+            this.Controls.Add(this.cboPhong);
+            this.Controls.Add(this.lblPhong);
+            this.Controls.Add(this.cboLoaiPhong);
+            this.Controls.Add(this.lblLoaiPhong);
+            this.Controls.Add(this.txtTen);
+            this.Controls.Add(this.lblTen);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblDuration);
+            this.Controls.Add(this.dtpEndDate);
+            this.Controls.Add(this.lblTo);
+            this.Controls.Add(this.lblFrom);
+            this.Controls.Add(this.dtpBeginDate);
             this.Name = "frmReservation";
             this.Text = "Reservation";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DateTimePicker dtpBeginDate;
+        private System.Windows.Forms.Label lblFrom;
+        private System.Windows.Forms.Label lblTo;
+        private System.Windows.Forms.DateTimePicker dtpEndDate;
+        private System.Windows.Forms.Label lblDuration;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblTen;
+        private System.Windows.Forms.TextBox txtTen;
+        private System.Windows.Forms.Label lblLoaiPhong;
+        private System.Windows.Forms.ComboBox cboLoaiPhong;
+        private System.Windows.Forms.ComboBox cboPhong;
+        private System.Windows.Forms.Label lblPhong;
     }
 }
