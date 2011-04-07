286042d7601519b287d4e19aad9e7206d02b85e4
0812611@gmail.com <0812611@gmail.com@df72b849-1963-1cf4-8301-46637306db84>
2011-04-07 04:10:45 +0000

    Sắp xếp các thư mục nộp bài. Ai nộp sai sẽ có hình phạt. Cụ thể: Source code sẽ commit vô thư mục mã nguồn. Các tài liệu chia làm 3 loại: TL yêu cầu, TL thiết kế, TL phát triển và sẽ commit vào các thư mục tương ứng. Ai không rõ thì lên forum xem hướng dẫn cụ thể, tránh làm sai.

--- WebService/DAO/PHONGDAO.cs
+++ WebService/DAO/PHONGDAO.cs
@@ -112,6 +112,134 @@ namespace DAO
                     link.Close();
             }
         }
+        public static void NhapPhong(string MP, string TP, string LP, string GC, string TT)
+        {
+
+            OleDbConnection link = null;
+            try
+            {
+                link = KetNoi();
+                string chuoiLenh = "insert into PHONG(MaPhong,TenPhong,MaLoaiPhong,TinhTrang,GhiChu) values(@MP,@Ten,@MLP,@TinhTrang,@GhiChu)";
+                OleDbCommand lenh = new OleDbCommand(chuoiLenh, link);
+
+                OleDbParameter thamSo = new OleDbParameter("@MP", OleDbType.LongVarChar);
+                thamSo.Value = MP;
+                lenh.Parameters.Add(thamSo);
+                thamSo = new OleDbParameter("@Ten", OleDbType.LongVarChar);
+                thamSo.Value = TP;
+                lenh.Parameters.Add(thamSo);
+                thamSo = new OleDbParameter("@MLP", OleDbType.LongVarChar);
+                thamSo.Value = LP;
+                lenh.Parameters.Add(thamSo);
+                thamSo = new OleDbParameter("@TinhTrang", OleDbType.LongVarChar);
+                thamSo.Value = TT;
+                lenh.Parameters.Add(thamSo);
+                thamSo = new OleDbParameter("@GhiChu", OleDbType.LongVarChar);
+                thamSo.Value = GC;
+                lenh.Parameters.Add(thamSo);
+                
+                lenh.ExecuteNonQuery();
+                OleDbDataAdapter Adapter = new OleDbDataAdapter();
+                Adapter.InsertCommand = lenh;
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                if (link != null && link.State == System.Data.ConnectionState.Open)
+                    link.Close();
+            }
+        }
+
+        public static PHONG LayPhong(string MP)
+        {
+            OleDbConnection link = null;
+            PHONG phg = new PHONG();
+            try
+            {
+                link = KetNoi();
+                string chuoiLenh = "Select * from PHONG , LOAIPHONG Where PHONG.MaLoaiPhong=LOAIPHONG.MaLoaiPhong and MaPhong=@MP";
+                OleDbCommand lenh = new OleDbCommand(chuoiLenh, link);
+                OleDbParameter thamSo = new OleDbParameter("@MP", OleDbType.LongVarChar);
+                thamSo.Value = MP;
+                lenh.Parameters.Add(thamSo);
+
+                OleDbDataReader Doc = lenh.ExecuteReader();
+                if (Doc.Read())
+                {
+                    phg.MaPhong = Doc.GetString(0);
+                    phg.TenPhong = Doc.GetString(1);
+                    LOAIPHONG loai = new LOAIPHONG();
+                    loai.MaLoaiPhong = Doc.GetString(2);
+                    loai.TenLoaiPhong = Doc.GetString(6);
+                    loai.DonGia = Doc.GetInt32(7);
+                    loai.SLKhachToiDa = Doc.GetInt32(8);
+                    //phg.LoaiPhong = loai;
+                    phg.MaLoaiPhong = loai.MaLoaiPhong;
+                    phg.GhiChu = Doc.GetString(3);
+                    phg.TinhTrang = Doc.GetString(4);
+                   // phg.SLKhach = Doc.GetInt32(5);
+                }
+                else phg.GhiChu = "Phong khong ton tai";
+            }
+            catch (Exception ex)
+            {
+                phg = new PHONG();
+            }
+            finally
+            {
+                if (link != null && link.State == System.Data.ConnectionState.Open)
+                    link.Close();
+            }
+            return phg;
+        }
+
+
+        public static void CapNhat(string MP, string TP, string LP, string GC, string TT)
+        {
+
+            OleDbConnection link = null;
+            try
+            {
+                link = KetNoi();
+                string chuoiLenh = "update PHONG set TenPhong=@TP,MaLoaiPhong=@LP,GhiChu=@GC,Tinhtrang=@TT where MaPhong=@MP";
+                OleDbCommand lenh = new OleDbCommand(chuoiLenh, link);
+
+                OleDbParameter thamSo = new OleDbParameter("@TP", OleDbType.LongVarChar);
+                thamSo.Value = TP;
+                lenh.Parameters.Add(thamSo);
+                thamSo = new OleDbParameter("@LP", OleDbType.LongVarChar);
+                thamSo.Value = LP;
+                lenh.Parameters.Add(thamSo);
+                thamSo = new OleDbParameter("@GC", OleDbType.LongVarChar);
+                thamSo.Value = GC;
+                lenh.Parameters.Add(thamSo);
+                thamSo = new OleDbParameter("@TT", OleDbType.LongVarChar);
+                thamSo.Value = TT;
+                lenh.Parameters.Add(thamSo);
+                
+                thamSo = new OleDbParameter("@MP", OleDbType.LongVarChar);
+                thamSo.Value = MP;
+                lenh.Parameters.Add(thamSo);
+                lenh.ExecuteNonQuery();
+                OleDbDataAdapter Adapter = new OleDbDataAdapter();
+                Adapter.UpdateCommand = lenh;
+
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                if (link != null && link.State == System.Data.ConnectionState.Open)
+                    link.Close();
+            }
+        }
+
+
         public static void XoaPhong(string MaPhong)
         {
             OleDbConnection link = null;
