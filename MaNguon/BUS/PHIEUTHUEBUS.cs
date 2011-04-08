1c6d201cc3e7f29c721102e1b31ec9e498e791d9
0812517@gmail.com <0812517@gmail.com@df72b849-1963-1cf4-8301-46637306db84>
2011-04-08 17:51:02 +0000

    xây dựng mã giả cho hàm ToiUuPhieuThue; author: 0812517.

--- MaNguon/BUS/PHIEUTHUEBUS.cs
+++ MaNguon/BUS/PHIEUTHUEBUS.cs
@@ -17,6 +17,12 @@ namespace BUS
             set { _loaiPhongCanToiUu = value; }
         }
 
+        static List<PHONG> _dsPhong = new List<PHONG>();
+
+        static List<PHIEUTHUE> _dsPhieuThue = new List<PHIEUTHUE>();
+
+        static int _chiSoPhongTrongMax = 0;
+
 
         public static bool ThemPhieu(PHIEUTHUE phieu)
         {
@@ -29,17 +35,73 @@ namespace BUS
 
         public static bool ToiUuPhieuThue(LOAIPHONG loaiPhong)
         {
+            //gán loại phòng cần tối ưu các phiếu thuê phòng
             PHIEUTHUEBUS.LoaiPhongCanToiUu = loaiPhong;
+
+            //lấy danh sách các phòng thuộc loại phòng đó
+            _dsPhong = PHONGBUS.LayDSPhongTheoLoaiPhong(LoaiPhongCanToiUu);
+
+            //lấy danh sách các phiếu thuê phòng thuộc loại phòng đó
+            for (int i = 0; i < _dsPhong.Count; i++)
+            {
+                _dsPhieuThue.AddRange(PHIEUTHUEBUS.LayDSPhieuThueTheoPhong(_dsPhong[i]));
+            }
+
             return TryToiUuPhieuThue(0);
         }
 
         private static bool TryToiUuPhieuThue(int phieuThue)
         {
-            List<PHONG> dsPhong = PHONGBUS.LayDSPhongTheoLoaiPhong(LoaiPhongCanToiUu);
-            for (int  i = 0; i < dsPhong.Count; i++)
+            if (phieuThue >= _dsPhieuThue.Count)
             {
+                //đã tối ưu hết phiếu thuê phòng
+                return true;
+            }
+            else
+            {
+                //xét lần lượt các phòng
+                for (int  i = 0; i < _dsPhong.Count; i++)
+                {
+                    //if (_dsPhieuThue[phieuThue] có thể chuyển tới _dsPhong[i])
+                    //{
+                        //Lưu lại phòng cũ của phiếu thuê;
+                        string maSoPhongCu = _dsPhieuThue[phieuThue].MaPhong;
+                        //Chuyển phiếu thuê sang phòng mới;
+                        _dsPhieuThue[phieuThue].MaPhong = _dsPhong[i].MaPhong;
+                        //Tính tổng chỉ số phòng trống;
+                        int chiSoPhongTrong = TinhChiSoPhongTrong();
+                        if (chiSoPhongTrong > _chiSoPhongTrongMax)
+                        {
+                            _chiSoPhongTrongMax = chiSoPhongTrong;
+                        }
+                        TryToiUuPhieuThue(phieuThue + 1);
+                        //Trả lại phòng cũ cho phiếu thuê i.
+                        _dsPhieuThue[phieuThue].MaPhong = maSoPhongCu;
+                    //}
+                }
             }
             return false;
+        }
+
+        private static int TinhChiSoPhongTrong()
+        {
+            //lấy ngày đầu tiên (làm chuẩn)
+            //lấy ngày cuối cùng (làm chuẩn)
+            //xét lần lượt các phòng
+            for (int i = 0; i < _dsPhong.Count; i++)
+            {
+                //xét lần lượt các phiếu thuê phòng của phòng đó
+                List<PHIEUTHUE> dsPhieuThuePhong = PHIEUTHUEBUS.LayDSPhieuThueTheoPhong(_dsPhong[i]);
+                for (int j = 0; j < dsPhieuThuePhong.Count; j++)
+                {
+                    //nếu là phiếu đầu tiên, xét nó với ngày đầu tiên => tính chỉ số
+                    //nếu không là phiếu đầu tiên, không phải là phiếu cuối cùng, xét nó với phiếu tiếp theo => tính chỉ số
+                    //nếu là phiếu cuối cùng, xét nó với ngày cuối cùng => tính chỉ số
+                }
+            }
+            //tính tổng các chỉ số
+            //trả về
+            return 0;
         }
     }
 }
