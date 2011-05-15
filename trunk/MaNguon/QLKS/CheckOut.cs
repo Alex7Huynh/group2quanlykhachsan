d0f5ed3b7ce2b3bc9d7fe6dfa658911e4b4f01af
0812033.luctanbinh@gmail.com <0812033.luctanbinh@gmail.com@df72b849-1963-1cf4-8301-46637306db84>
2011-05-15 19:25:27 +0000

    edit checkout form -0812033

--- trunk/MaNguon/QLKS/CheckOut.cs
+++ trunk/MaNguon/QLKS/CheckOut.cs
@@ -15,65 +15,28 @@ namespace QLKS
         public frmCheckOut()
         {
             InitializeComponent();
-            KhoiTaoComboBox();
         }
 
-        public void KhoiTaoComboBox()
+        private void rdoTenPhong_CheckedChanged(object sender, EventArgs e)
         {
-            List<PHIEUTHUE> phieuThue = new List<PHIEUTHUE>();
-            PHONG phong = new PHONG();
-            phong.TinhTrang = "checkin";
-            phieuThue = PHIEUTHUEBUS.LayDSPhieuThueTheoTinhTrangPhong(phong);
-            for (int i = 0; i < phieuThue.Count; i++)
+            if (rdoTenPhong.Checked)
             {
-                comboBox1.Items.Add(phieuThue[i].MaPhong);
+                txtTenPhong.ReadOnly = false;
+                txtCMND1.ReadOnly = true;
+                gbThanhTien.Enabled = false;
             }
-        }
-        private void button1_Click(object sender, EventArgs e)
-        {
-            try
+            else
             {
-                PHONG phong = new PHONG();
-                phong.MaPhong = comboBox1.SelectedItem.ToString();
-                phong.TinhTrang = "free";
-                bool b = PHONGBUS.UpdateTinhTrangPhong(phong);
-                if (true == b)
-                {
-                    int SoNgayO = PHIEUTHUEBUS.layTongSoNgayThue(phong);
-                    int DonGia = PHONGBUS.LayDonGiaTheoPhong(phong);
-                    long TongTien = SoNgayO * DonGia;
-                    textBox1.Text = TongTien.ToString()+"USD";
-                    MessageBox.Show("Check out thành công");
-                    dataGridView1.Rows.Clear();
-                    //CheckOut ck = new CheckOut();
-                    comboBox1.Items.RemoveAt(comboBox1.SelectedIndex);
-                    comboBox1.Items.Clear();
-                    KhoiTaoComboBox();
-
-                }
-                else
-                    return;
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Check out thất bại hoàn toàn");
+                txtTenPhong.ReadOnly = true;
+                txtCMND1.ReadOnly = false;
             }
         }
 
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        private void frmCheckOut_Load(object sender, EventArgs e)
         {
-            PHONG phong = new PHONG();
-            phong.MaPhong = comboBox1.SelectedItem.ToString();
-            // MessageBox.Show(phong.MaPhong);
-            List<PHIEUTHUE> phieuThue = new List<PHIEUTHUE>();
-            phieuThue = PHIEUTHUEBUS.LayDSPhieuThueTheoPhong(phong);
-            for (int i = 0; i < phieuThue.Count; i++)
-            {
-                dataGridView1.Rows.Add(phieuThue[i].MaPhong, phieuThue[i].MaPhieuThue, phieuThue[i].NgayThue, phieuThue[i].SoNgayThue, phieuThue[i].TenKhachHangDaiDien);
-                dataGridView1.Rows[dataGridView1.RowCount - 1].Tag = phieuThue[i];
-            }
-            
-         
+
         }
+
+        
     }
 }
