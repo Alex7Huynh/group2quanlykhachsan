e89412b82d4beddf40ca1d72c6fedb45385a4426
0812033.luctanbinh@gmail.com <0812033.luctanbinh@gmail.com@df72b849-1963-1cf4-8301-46637306db84>
2011-05-15 18:40:28 +0000

    edit checkin form - 0812033

--- trunk/MaNguon/QLKS/Checkin.Designer.cs
+++ trunk/MaNguon/QLKS/Checkin.Designer.cs
@@ -29,91 +29,119 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtCMND = new System.Windows.Forms.TextBox();
+            this.btnNhanPhong = new System.Windows.Forms.Button();
             this.btnThoat = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.dgvDanhSachPhieuThue = new System.Windows.Forms.DataGridView();
+            this.maPhong = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.maPhieuThue = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ngayThue = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.soNgayThue = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tenKH = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDanhSachPhieuThue)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(214, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(95, 16);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Người Checkin";
+            this.label1.Size = new System.Drawing.Size(133, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nhận Phòng";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(51, 82);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(109, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "CMND/PassPort";
+            // 
+            // txtCMND
+            // 
+            this.txtCMND.Location = new System.Drawing.Point(277, 82);
+            this.txtCMND.Name = "txtCMND";
+            this.txtCMND.Size = new System.Drawing.Size(200, 20);
+            this.txtCMND.TabIndex = 7;
+            // 
+            // btnNhanPhong
+            // 
+            this.btnNhanPhong.Location = new System.Drawing.Point(114, 449);
+            this.btnNhanPhong.Name = "btnNhanPhong";
+            this.btnNhanPhong.Size = new System.Drawing.Size(75, 23);
+            this.btnNhanPhong.TabIndex = 9;
+            this.btnNhanPhong.Text = "Nhận Phòng";
+            this.btnNhanPhong.UseVisualStyleBackColor = true;
             // 
             // btnThoat
             // 
-            this.btnThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnThoat.Location = new System.Drawing.Point(343, 9);
+            this.btnThoat.Location = new System.Drawing.Point(330, 449);
             this.btnThoat.Name = "btnThoat";
-            this.btnThoat.Size = new System.Drawing.Size(65, 23);
-            this.btnThoat.TabIndex = 6;
-            this.btnThoat.Text = "Thoát";
+            this.btnThoat.Size = new System.Drawing.Size(75, 23);
+            this.btnThoat.TabIndex = 10;
+            this.btnThoat.Text = "Thoát";
             this.btnThoat.UseVisualStyleBackColor = true;
-            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
             // 
-            // button1
+            // dgvDanhSachPhieuThue
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(272, 9);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(65, 23);
-            this.button1.TabIndex = 7;
-            this.button1.Text = "Checkin";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            this.dgvDanhSachPhieuThue.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDanhSachPhieuThue.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.maPhong,
+            this.maPhieuThue,
+            this.ngayThue,
+            this.soNgayThue,
+            this.tenKH});
+            this.dgvDanhSachPhieuThue.Location = new System.Drawing.Point(20, 167);
+            this.dgvDanhSachPhieuThue.Name = "dgvDanhSachPhieuThue";
+            this.dgvDanhSachPhieuThue.RowHeadersVisible = false;
+            this.dgvDanhSachPhieuThue.Size = new System.Drawing.Size(502, 253);
+            this.dgvDanhSachPhieuThue.TabIndex = 12;
             // 
-            // label2
+            // maPhong
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(164, 52);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(230, 20);
-            this.label2.TabIndex = 8;
-            this.label2.Text = "Thông tin phiếu thuê tương ứng";
+            this.maPhong.HeaderText = "Mã phòng";
+            this.maPhong.Name = "maPhong";
             // 
-            // dataGridView1
+            // maPhieuThue
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(4, 94);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.None;
-            this.dataGridView1.Size = new System.Drawing.Size(534, 74);
-            this.dataGridView1.TabIndex = 9;
+            this.maPhieuThue.HeaderText = "Mã phiếu thuê";
+            this.maPhieuThue.Name = "maPhieuThue";
             // 
-            // comboBox1
+            // ngayThue
             // 
-            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(113, 9);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(139, 21);
-            this.comboBox1.TabIndex = 10;
-            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged_1);
+            this.ngayThue.HeaderText = "Ngày thuê";
+            this.ngayThue.Name = "ngayThue";
+            // 
+            // soNgayThue
+            // 
+            this.soNgayThue.HeaderText = "Số ngày thuê";
+            this.soNgayThue.Name = "soNgayThue";
+            // 
+            // tenKH
+            // 
+            this.tenKH.HeaderText = "Tên khách đại diện";
+            this.tenKH.Name = "tenKH";
             // 
             // frmCheckin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(543, 262);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(543, 484);
+            this.Controls.Add(this.dgvDanhSachPhieuThue);
             this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnNhanPhong);
+            this.Controls.Add(this.txtCMND);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label1);
             this.Name = "frmCheckin";
-            this.Text = "Checkin";
-            this.Load += new System.EventHandler(this.frmCheckin_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Text = "Nhận Phòng";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDanhSachPhieuThue)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -122,11 +150,18 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtCMND;
+        private System.Windows.Forms.Button btnNhanPhong;
         private System.Windows.Forms.Button btnThoat;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dgvDanhSachPhieuThue;
+        private System.Windows.Forms.DataGridViewTextBoxColumn maPhong;
+        private System.Windows.Forms.DataGridViewTextBoxColumn maPhieuThue;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ngayThue;
+        private System.Windows.Forms.DataGridViewTextBoxColumn soNgayThue;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tenKH;
+
+
 
     }
 }
--- trunk/MaNguon/QLKS/Checkin.cs
+++ trunk/MaNguon/QLKS/Checkin.cs
@@ -20,56 +20,6 @@ namespace QLKS
             
         }                      
 
-        private void button1_Click_1(object sender, EventArgs e)
-        {
-            try
-            {
-                ptCapNhat = PHIEUTHUEBUS.LayPhieuThueTheoMa(comboBox1.SelectedValue.ToString())[0];
-                PHIEUTHUEBUS.UpdateCheckin(ptCapNhat, true);
-                MessageBox.Show("check in thành công");
-
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            finally
-            {
-
-                phieuThue = PHIEUTHUEBUS.LayDSPhieuThueChuaCheckin();
-                comboBox1.DataSource = phieuThue;
-                comboBox1.DisplayMember = "tenKhachHangDaiDien";
-                comboBox1.ValueMember = "maPhieuThue";
-                comboBox1.SelectedIndex = 1;           }
-           
-        }
-
-        private void btnThoat_Click(object sender, EventArgs e)
-        {
-            Close();
-        }
-
-        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
-        {
-            try
-            {
-                if (comboBox1.SelectedIndex > -1)
-                    dataGridView1.DataSource = PHIEUTHUEBUS.LayPhieuThueTheoMa(comboBox1.SelectedValue.ToString());
-
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
-
-        private void frmCheckin_Load(object sender, EventArgs e)
-        {
-            phieuThue = PHIEUTHUEBUS.LayDSPhieuThueChuaCheckin();
-            comboBox1.DataSource = phieuThue; 
-            comboBox1.DisplayMember = "tenKhachHangDaiDien";
-            comboBox1.ValueMember = "maPhieuThue";
-            comboBox1.SelectedIndex = 1;
-        }
+        
     }
 }
