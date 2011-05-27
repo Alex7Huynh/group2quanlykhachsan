ab77d73dde9eae4feda7e9a400c5f7b55832b281
0812033.luctanbinh@gmail.com <0812033.luctanbinh@gmail.com@df72b849-1963-1cf4-8301-46637306db84>
2011-05-27 15:39:45 +0000

    Thêm lớp QLKSBUS - 0812033

---
+++ trunk/MaNguonChinh/BUS/QLKSBUS.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DAO;
+
+namespace BUS
+{
+    public class QLKSBUS
+    {
+        public static void KiemTraKetNoi()
+        {
+            try
+            {
+                if (ABSDAO.KetNoi() != null)
+                {
+                    return;
+                }
+            }
+            catch (System.Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
